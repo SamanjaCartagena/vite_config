c18af283c07cf159900042ef6d14d57df5e65df2
SamanjaCartagena <cart.samanja09@gmail.com>
2023-01-28 17:51:40 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({mode})=>{
+
+ const env = loadEnv(mode, process.cwd());
+  
+  return{
+    plugins: [react()],
+   build:{
+       outDir:"./wwwroot/app/", sourcemap:true
+    },
+  server: {
+    
+
+     port:env.VITE_PORT
+  }
+}
+})
